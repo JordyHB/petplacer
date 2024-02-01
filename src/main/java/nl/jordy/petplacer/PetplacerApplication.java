3e58e66f97abfbd3bbb3d3e9fd4723386402e210
jord1990 <jordy.hoogeboom@hotmail.nl>
2024-02-01 18:31:45 +0100

    initial commit

---
+++ src/main/java/nl/jordy/petplacer/PetplacerApplication.java
@@ -0,0 +1,13 @@
+package nl.jordy.petplacer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetplacerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetplacerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/jordy/petplacer/PetplacerApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.jordy.petplacer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetplacerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
