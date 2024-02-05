44ace0c443e21ba715935fe31df61245d3b3ed91
jord1990 <jordy.hoogeboom@hotmail.nl>
2024-02-05 21:28:54 +0100

    connected to database

---
+++ src/main/java/nl/jordy/petplacer/models/ShelterManager.java
@@ -0,0 +1,10 @@
+//package nl.jordy.petplacer.models;
+//
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.Table;
+//
+//@Entity
+//@Table(name = "shelter_managers")
+//public class ShelterManager extends User {
+//
+//}
---
+++ src/main/java/nl/jordy/petplacer/models/User.java
@@ -0,0 +1,9 @@
+//package nl.jordy.petplacer.models;
+//
+//
+//import jakarta.persistence.MappedSuperclass;
+//
+//@MappedSuperclass
+//public class User {
+//
+//}
