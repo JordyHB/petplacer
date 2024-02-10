3e59057a8180982e20bcc4e4b26fd42031458829
jord1990 <jordy.hoogeboom@hotmail.nl>
2024-02-10 15:16:14 +0100

    implemented the usage of a DTO and added validation

--- src/main/java/nl/jordy/petplacer/controllers/UserController.java
+++ src/main/java/nl/jordy/petplacer/controllers/UserController.java
@@ -1,31 +1,43 @@
 package nl.jordy.petplacer.controllers;
 
 
+import jakarta.validation.Valid;
+import nl.jordy.petplacer.dtos.UserDto;
+import nl.jordy.petplacer.services.UserService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import nl.jordy.petplacer.models.User;
-import nl.jordy.petplacer.repositories.UserRepository;
 
 @RestController
 public class UserController {
 
-    private final UserRepository userRepository;
+    private final UserService userService;
 
-    public UserController(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
 
     @PostMapping("/register")
-        public ResponseEntity<User> registerUser(@RequestBody User user) {
+        public ResponseEntity<String> registerUser(
+                @Valid
+                @RequestBody UserDto userDto,
+                BindingResult bindingResult
+    ) {
 
-
-            // Save the user to the database
-            User savedUser = userRepository.save(user);
-
-            return ResponseEntity.ok(savedUser);
+        //catches errors and sends them back to the user
+        if (bindingResult.hasErrors()) {
+            StringBuilder errors = new StringBuilder();
+            for (FieldError error : bindingResult.getFieldErrors()) {
+                errors.append(error.getField()).append(": ").append(error.getDefaultMessage()).append("\n");
+            }
+            return ResponseEntity.badRequest().body(errors.toString());
         }
+
+        return ResponseEntity.ok(userService.registerUser(userDto));
+    }
 
 }
--- src/main/java/nl/jordy/petplacer/services/UserService.java
+++ src/main/java/nl/jordy/petplacer/services/UserService.java
@@ -1,5 +1,6 @@
 package nl.jordy.petplacer.services;
 
+import nl.jordy.petplacer.dtos.UserDto;
 import nl.jordy.petplacer.models.User;
 import nl.jordy.petplacer.repositories.UserRepository;
 import org.springframework.stereotype.Service;
@@ -7,13 +8,29 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
     private final UserRepository userRepository;
+
+    //Injects dependencies
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
-    public User registerUser(User user) {
-        return userRepository.save(user);
+    public String registerUser(UserDto userDto) {
+
+        //Creates user class and fills it with data from the DTO
+        User user = new User();
+
+        user.setUsername(userDto.getUsername());
+        user.setFirstName(userDto.getFirstName());
+        user.setLastName(userDto.getLastName());
+        user.setEmail(userDto.getEmail());
+
+        //Saves it and returns the username
+        User savedUser = userRepository.save(user);
+
+        return savedUser.getUsername();
+
     }
+
 }
 
 
