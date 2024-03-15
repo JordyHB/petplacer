2b4d68d07f8e645ba8455104b03b605124116588
jord1990 <jordy.hoogeboom@hotmail.nl>
2024-03-15 17:04:36 +0100

    added an abstract super class and 2 entities that extend the super

---
+++ src/main/java/nl/jordy/petplacer/models/Pet.java
@@ -0,0 +1,18 @@
+package nl.jordy.petplacer.models;
+
+public abstract class Pet {
+
+    private String name;
+    private String species;
+    private String breed;
+    private String color;
+    private int age;
+    private char gender;
+    private String size;
+    private String description;
+    private boolean spayedNeutered;
+    private boolean goodWithKids;
+    private boolean goodWithDogs;
+    private boolean goodWithCats;
+
+}
---
+++ src/main/java/nl/jordy/petplacer/models/ShelterPet.java
@@ -0,0 +1,28 @@
+package nl.jordy.petplacer.models;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "shelter_pets")
+public class ShelterPet extends Pet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    private Long id;
+
+    private String status;
+    private Date dateOfArrival;
+    private int monthsInShelter;
+    private String medicalHistory;
+    private String specialNeeds;
+    private String previousSituation;
+
+}
