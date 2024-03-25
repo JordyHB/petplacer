8ef4360164216393fb94ba2ef00a77effed8df32
jord1990 <jordy.hoogeboom@hotmail.nl>
2024-03-25 19:46:46 +0100

    added logic that checks wheter user is manager of specific shelter or admin and implemented it

--- src/main/java/nl/jordy/petplacer/services/ShelterService.java
+++ src/main/java/nl/jordy/petplacer/services/ShelterService.java
@@ -1,7 +1,6 @@
 package nl.jordy.petplacer.services;
 
 import nl.jordy.petplacer.dtos.input.ShelterInputDTO;
-import nl.jordy.petplacer.dtos.input.UserInputDTO;
 import nl.jordy.petplacer.dtos.output.ShelterOutputDTO;
 import nl.jordy.petplacer.dtos.patch.ShelterPatchDTO;
 import nl.jordy.petplacer.exceptions.AlreadyExistsException;
@@ -12,6 +11,7 @@ import nl.jordy.petplacer.models.Authority;
 import nl.jordy.petplacer.models.Shelter;
 import nl.jordy.petplacer.models.User;
 import nl.jordy.petplacer.repositories.ShelterRepository;
+import nl.jordy.petplacer.util.AccessValidator;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -22,10 +22,15 @@ public class ShelterService {
 
     private final ShelterRepository shelterRepository;
     private final UserService userService;
+    private final AccessValidator accessValidator;
 
-    public ShelterService(ShelterRepository shelterRepository, UserService userService) {
+    public ShelterService(ShelterRepository shelterRepository,
+                          UserService userService,
+                          AccessValidator accessValidator
+    ) {
         this.userService = userService;
         this.shelterRepository = shelterRepository;
+        this.accessValidator = accessValidator;
     }
 
     public Shelter fetchShelterByID(Long shelterID) {
@@ -90,7 +95,9 @@ public class ShelterService {
 
     public ShelterOutputDTO updateShelterByID(Long shelterID, ShelterPatchDTO shelterPatchDTO) {
 
+        // validates user is allowed to update the shelter and that shelter name is unique
         validateShelterNameUnique(shelterPatchDTO);
+        accessValidator.isSheltersManagerOrAdmin(AccessValidator.getAuth(), shelterID);
 
         Shelter shelter = fetchShelterByID(shelterID);
 
@@ -104,12 +111,17 @@ public class ShelterService {
     }
 
     public String deleteShelterByID(Long shelterID) {
+
+        accessValidator.isSheltersManagerOrAdmin(AccessValidator.getAuth(), shelterID);
+
         // uses private method to fetch and validate the user exists
         shelterRepository.delete(fetchShelterByID(shelterID));
         return "Shelter: " + shelterID + " has been successfully deleted.";
     }
 
     public ShelterOutputDTO addManagerToShelter(Long shelterID, String username) {
+
+        accessValidator.isSheltersManagerOrAdmin(AccessValidator.getAuth(), shelterID);
 
         Shelter shelter = fetchShelterByID(shelterID);
         User user = userService.fetchUserByUsername(username);
