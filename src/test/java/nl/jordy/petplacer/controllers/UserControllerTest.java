dfc5f37ba0c0eb878627ff3e478ec57d65d3a816
jord1990 <jordy.hoogeboom@hotmail.nl>
2024-04-18 17:50:42 +0200

    added a test for an existing user and a test for adding shelters

--- src/test/java/nl/jordy/petplacer/controllers/UserControllerTest.java
+++ src/test/java/nl/jordy/petplacer/controllers/UserControllerTest.java
@@ -2,8 +2,10 @@ package nl.jordy.petplacer.controllers;
 
 import nl.jordy.petplacer.dtos.input.UserInputDTO;
 import nl.jordy.petplacer.dtos.output.UserOutputDTO;
+import nl.jordy.petplacer.exceptions.AlreadyExistsException;
 import nl.jordy.petplacer.services.JwtService;
 import nl.jordy.petplacer.services.UserService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -34,6 +36,7 @@ class UserControllerTest {
     private MockMvc mockMvc;
 
 
+    @DisplayName("Register a user successfully")
     @Test
     void registerUser() throws Exception {
         // Arrange
@@ -50,8 +53,8 @@ class UserControllerTest {
 
         // Act & Assert
         MvcResult mvcResult = this.mockMvc.perform(MockMvcRequestBuilders.post("/users")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(requestJson))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestJson))
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.username").value("emel"))
@@ -63,8 +66,63 @@ class UserControllerTest {
                 matchesPattern("^.*/users/emel$"));
     }
 
+    @DisplayName("register a user with an existing username")
     @Test
-    void registerShelter() {
+void registerUserWithExistingUsername() throws Exception {
+        // Arrange
+        String requestJson = """
+                {
+                "username": "jord",
+                "password": "5678",
+                "firstName": "jordy",
+                "lastName": "doe",
+                "email": "jord@example.com",
+                "phoneNumber": "0656464462"
+                }
+                """;
+
+        // Act & Assert
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestJson))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isConflict());
+    }
+
+
+
+    @DisplayName("Register a shelter successfully")
+    @Test
+    void registerShelter() throws Exception {
+
+        // Arrange
+        String requestJson =
+                """
+                        {
+                                "shelterName":"Example Shelter",
+                                "phoneNumber":"0612345678",
+                                "email":"example@example.com",
+                                "address":"123 Example Street",
+                                "city":"Example City",
+                                "postalCode":"1234AB",
+                                "description":"This is a description of the shelter.",
+                                "website":"https://www.example.com",
+                                "facilities":"Facility 1, Facility 2, Facility 3",
+                                "openingHours":"Monday to Friday: 9am to 5pm"
+                        }
+                                """;
+
+        // Act & Assert
+        MvcResult mvcResult = this.mockMvc.perform(MockMvcRequestBuilders.post("/users/jord/shelters")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestJson))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.shelterName").value("example shelter"))
+                .andExpect(jsonPath("$.dateOfRegistration").exists())
+                .andExpect(jsonPath("$.managers[0].username").value("jord"))
+                .andReturn();
+
     }
 
     @Test
